d016e8aad96ef0eb99060a325cf6d1c6d58cb133
Alexis Munsayac <alexis.munsayac@gmail.com>
2020-12-08 22:21:50 +0800

    Update example to use stress test

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,46 +1,88 @@
 import React, { Suspense } from 'react';
 import { createExternalSubject, useExternalSubject } from 'react-external-subject';
 
-// An example of a mutable source
-let source = 0;
+const increment = document.getElementById('increment');
+const incrementMulti = document.getElementById('increment-multi');
+const incrementAuto = document.getElementById('increment-auto');
+
+class Counter {
+  private value = 0;
+
+  private listeners = new Set<() => void>();
+
+  get state() {
+    return this.value;
+  }
+
+  set state(value: number) {
+    this.value = value;
+
+    this.listeners.forEach((listener) => {
+      listener();
+    });
+  }
+
+  subscribe(listener: () => void): () => void {
+    this.listeners.add(listener);
+    return () => {
+      this.listeners.delete(listener);
+    };
+  }
+};
+
+const source = new Counter();
+
+increment?.addEventListener('click', () => {
+  source.state += 1;
+});
+incrementMulti?.addEventListener('click', () => {
+  source.state += 1;
+  source.state += 1;
+});
+
+let raf: number | undefined;
+
+function begin() {
+  source.state += 1;
+  raf = requestAnimationFrame(begin);
+}
+
+incrementAuto?.addEventListener('click', () => {
+  if (raf) {
+    cancelAnimationFrame(raf);
+    raf = undefined;
+  } else {
+    raf = requestAnimationFrame(begin);
+  }
+});
 
 // Wrap our mutable source into an external subject
 const subject = createExternalSubject({
-  read: () => source,
+  read: () => source.state,
+  subscribe: (handler) => source.subscribe(handler),
 });
 
-// Create a component that reads from our mutable source
-function ObserverA() {
-  const value = useExternalSubject(subject, false);
-  return <h1>Value: {value}</h1>;
-}
-
-// Create a component similar to ObserverA but suspends
-function ObserverB() {
+function Observer() {
   const value = useExternalSubject(subject, true);
-  return <h1>Value: {value}</h1>;
+  return <div>{value}</div>;
 }
 
-// Create a component with a render-time side-effect
-function Effectful() {
-  // Update our source
-  source = 1337;
-  return <h1>Effectful: {source}</h1>;
+function hash() {
+  // Math.random should be unique because of its seeding algorithm.
+  // Convert it to base 36 (numbers + letters), and grab the first 9 characters
+  // after the decimal.
+  return '_' + Math.random().toString(36).substr(2, 9);
 }
 
 function App() {
+  const items = [];
+  for (let i = 0; i < 500; i += 1) {
+    items.push(<Observer key={hash()} />)
+  }
   return (
-    <>
-      <ObserverA />
-      <Effectful />
-      {/*
-        By the time ObserverB renders, the source has already 
-        updated, creating a "tear".
-      */}
-      <Suspense fallback={null}>
-        <ObserverB />
-      </Suspense>
-    </>
+    <Suspense fallback={null}>
+      {items}
+    </Suspense>
   );
 }
 
