081c47358f7a7fc2989cce3e30924ad5858e1552
Alexis Munsayac <alexis.munsayac@gmail.com>
2020-12-15 00:23:15 +0800

    Update create-external-subject.ts

--- src/create-external-subject.ts
+++ src/create-external-subject.ts
@@ -63,6 +63,8 @@ export default function createExternalSubject<T>(
                 });
                 resolve();
               });
+            } else {
+              resolve();
             }
           }
         })),
